8050324d612af34659d8aaeccd485682995b8a97
BoomPowBep <37978668+BoomPowbep@users.noreply.github.com>
2019-01-04 10:17:00 +0100

    Added Light & Dark mode support

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const Encore = require('@symfony/webpack-encore');
-const path   = require('path');
+const path = require('path');
 
 const assets_path = path.resolve('./src');
 const output_path = path.resolve('./public/build');
 const public_path = '/build';
-const js_path     = path.join(assets_path, './js');
-const scss_path   = path.join(assets_path, './scss');
+const js_path = path.join(assets_path, './js');
+const scss_core_path = path.join(assets_path, './scss/core');
+const scss_dark_path = path.join(assets_path, './scss/variants/dark');
+const scss_light_path = path.join(assets_path, './scss/variants/light');
 
 Encore
     .setOutputPath(output_path)
@@ -13,7 +15,15 @@ Encore
 
     .addEntry('app', [
         path.join(js_path, '/main.js'),
-        path.join(scss_path, '/style.scss')
+        path.join(scss_core_path, '/style.scss'),
+    ])
+
+    .addStyleEntry('dark', [
+        path.join(scss_dark_path, '/style.scss')
+    ])
+
+    .addStyleEntry('light', [
+        path.join(scss_light_path, '/style.scss')
     ])
 
     .enableSassLoader()
@@ -27,6 +37,6 @@ Encore
 ;
 
 let config = Encore.getWebpackConfig();
-config.watchOptions = { poll: true, ignored: /node_modules/ };
+config.watchOptions = {poll: true, ignored: /node_modules/};
 
 module.exports = config;
